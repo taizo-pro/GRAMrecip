546a703466fadce55f77defb0c29813effc884fe
harada_k <t2s.single@gmail.com>
2020-09-08 16:57:59 +0900

    homeコントローラ作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+    # ユーザーがページにアクセスする前に、これを呼び出す
+    before_action :configure_permitted_parameters, if: :devise_controller?
+    # このコントローラ内でしか呼ばれないようにする
+    private
+    def configure_permitted_parameters
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+    end
 end
