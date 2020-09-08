a4aacb61caf1bd1cf7415bb644762db4e987d4ac
harada_k <t2s.single@gmail.com>
2020-09-08 19:32:34 +0900

    ユーザー詳細画面の機能作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,15 @@
 class UsersController < ApplicationController
+  # ユーザーの一覧画面
   def index
     # Userモデルの情報を全て取得する
     @users = User.all
   end
 
+  # ユーザーの詳細画面
   def show
+    # 1人だけの情報を取り出す
+    # params[:id]で、URLの番号をとってくる
+    @user = User.find(params[:id])
   end
 
   def edit
