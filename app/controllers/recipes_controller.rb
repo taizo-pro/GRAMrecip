288da5160e17a0283b6e9dc94f7b20e105f25861
harada_k <t2s.single@gmail.com>
2020-09-09 12:06:50 +0900

    新規登録できる機能を追加

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -10,6 +10,19 @@ class RecipesController < ApplicationController
     @recipe = Recipe.new
   end
 
+  def create
+    @recipe = Recipe.new(recipe_params)
+    # ログインしてる人のIDを判別する。誰が投稿しているのか
+    @recipe.user_id = current_user.id
+    @recipe.save
+    redirect_to recipe_path(@recipe)
+  end
+
+  private
+  def recipe_params
+    params.require(:recipe).permit(:title, :body, :image)
+  end
+
   def edit
   end
 end
